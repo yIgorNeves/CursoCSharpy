80863226087ee3b01ded497a921f1fc0b9744207
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2020-05-19 15:32:16 -0300

    Enum

--- Store/Store/Program.cs
+++ Store/Store/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Store.Entities;
+using Store.Entities.Enum;
+using System;
 
 namespace Store
 {
@@ -6,7 +8,43 @@ namespace Store
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date(DD/MM/YYYY): ");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthdate);            
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status (PendingPayment, Processing, Shipped, Delivered) : ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i+1} item data:");
+                Console.Write("Product name: ");
+                string Pname = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quant = int.Parse(Console.ReadLine());
+
+                Product product = new Product(Pname, price);
+                OrderItem item = new OrderItem(quant, price, product);
+
+                order.AddItem(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Order Sumary");
+            Console.WriteLine(order);
         }
     }
 }
