8ecf889677dce55420318cc9824c4526f7b5f622
Igor Neves <32842877+yIgorNeves@users.noreply.github.com>
2020-05-19 11:17:00 -0300

    exercicios

---
+++ Worker/Worker/Entities/Depatarment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerContracts.Entities
+{
+    class Depatarment
+    {
+        public string Name { get; set; }
+
+        public Depatarment()
+        {
+        }
+
+        public Depatarment(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Worker/Worker/Entities/Worker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using WorkerContracts.Entities.Enums;
+using System.Collections.Generic;
+
+namespace WorkerContracts.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Depatarment Depatarment { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Depatarment departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Depatarment = departament;
+        }
+
+        public void AddContracts(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+
+            return sum;
+        }
+    }
+}
